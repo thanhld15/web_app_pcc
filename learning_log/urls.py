397a781ee1057cc6de040a3ddec371632ced5b9d
ThXnH <thanh.ld2303@gmail.com>
2022-11-15 19:59:27 +0700

    Finish part 1 of Chapter 19

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('learning_logs.urls', 'learning_logs'), 
-            namespace='learning_logs')),
+    path("admin/", admin.site.urls),
+    path(
+        "", include(("learning_logs.urls", "learning_logs"), namespace="learning_logs")
+    ),
 ]
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,13 +6,16 @@ from . import views
 
 urlpatterns = [
     # Home page
-    path('', views.index, name='index'),
-    path('topics/', views.topics, name='topics'),
-
+    path("", views.index, name="index"),
+    path("topics/", views.topics, name="topics"),
     # Detail page for each topic
-    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
-
+    path(r"^topics/(?P<topic_id>\d+)/$", views.topic, name="topic"),
     # Page for adding new topic
-    path('new_topic/', views.new_topic, name='new_topic')
+    path("new_topic/", views.new_topic, name="new_topic"),
+    # Page for adding new entry
+    path(r"^new_entry/(?P<topic_id>\d+)/$", views.new_entry, name='new_entry'),
+    # Page for editing an entry
+    path(r"^edit_entry/(?P<entry_id>\d+)/$", views.edit_entry, 
+        name='edit_entry'),
     
 ]
