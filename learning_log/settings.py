06a4ebb136ad49189b070213874d28e8d0fc98c8
ThXnH <thanh.ld2303@gmail.com>
2022-11-19 18:48:06 +0700

    Finish chapter 19

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# My settings
+LOGIN_URL = '/users/login/'
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # My apps
     "learning_logs",
+    "users",
 ]
 
 MIDDLEWARE = [
